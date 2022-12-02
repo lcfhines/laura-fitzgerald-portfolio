a62290ef22872f98d1891a6a7d41fadda3037f0d
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-02 18:28:52 -0500

    set up components and started styling header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation'
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+        <h1>Laura Fitzgerald</h1>
+        <Navigation />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav class="navbar navbar-expand-lg">
+        <div>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    About Me
+                </li>
+                <li class="nav-item active">
+                    Portfolio</li>
+                <li class="nav-item active">
+                    Contact
+                </li>
+                <li class="nav-item active">
+                    Resume
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+  )
+}
+
+export default Navigation
