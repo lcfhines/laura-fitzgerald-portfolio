deb3f238b3b6022c4994bd2c12dd0106eae28798
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-02 19:14:50 -0500

    added Medium link to footer as third party

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,6 +19,7 @@ const Footer = () => {
       <ul className='navbar-nav'>
         <li className='footer-link nav-item'><a href="https://github.com/lcfhines" target="_blank">GitHub</a></li>
         <li className='footer-link nav-item'><a href="https://www.linkedin.com/in/laura-fitzgerald-4ab69572/" target="_blank">LinkedIn</a></li>
+        <li className='footer-link nav-item'><a href="https://medium.com/@lcfhines" target="_blank">Medium</a></li>
       </ul>      
     </footer>
     
